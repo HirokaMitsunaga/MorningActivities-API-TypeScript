05fc5ddc016ab2e365cf7440d5c4182b4dff3065
hiroka <mitsunagahrk87@gmail.com>
2024-12-27 14:33:50 +0900

    hono hello world

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("aieo");
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { prettyJSON } from "hono/pretty-json";
+
+const app = new Hono();
+
+//ミドルウェアの設定
+app.use("*", prettyJSON());
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+const port = 3000;
+console.log(`Server is running on http://localhost:${port}`);
+
+serve({
+  fetch: app.fetch,
+  port,
+});
