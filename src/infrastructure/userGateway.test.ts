13715cd3e80f7226c57feb2b9c59b86f7863a3ac
hiroka <mitsunagahrk87@gmail.com>
2025-01-02 17:31:08 +0900

    getUserByEmailのテスト

--- src/infrastructure/userGateway.test.ts
+++ src/infrastructure/userGateway.test.ts
@@ -9,77 +9,113 @@ describe("UserGateway", () => {
     userGateway = new UserGateway(prismaMock);
   });
 
-  it("should insert a user successfully", async () => {
-    const userData: UserPostRequestBody = {
-      email: "test@example.com",
-      name: "Test User",
-      password: "password123",
-    };
+  describe("UserGateway insert", () => {
+    it("ユーザの作成に成功する", async () => {
+      const userData: UserPostRequestBody = {
+        email: "test@example.com",
+        name: "Test User",
+        password: "password123",
+      };
 
-    const expectedUser = {
-      id: 1,
-      ...userData,
-      created_at: new Date(),
-      updated_at: new Date(),
-    };
-    prismaMock.user.create.mockResolvedValue(expectedUser);
+      const expectedUser = {
+        id: 1,
+        ...userData,
+        created_at: new Date(),
+        updated_at: new Date(),
+      };
+      prismaMock.user.create.mockResolvedValue(expectedUser);
+      const userRecord = await userGateway.insert(userData);
+      expect(userRecord).toEqual(expectedUser);
+      expect(prismaMock.user.create).toHaveBeenCalledWith({ data: userData });
+    });
 
-    const userRecord = await userGateway.insert(userData);
+    // TODO　ユーニーク制約のテストを書く
+    // 下記で同じemaiを登録したらユニークキー制約でエラーになるテストを書いたが、違うemaiで登録してもテストが通ってしまうので改善が必要
+    // it("should throw an error when inserting fails due to unique constraint", async () => {
+    //   const userData1: UserPostRequestBody = {
+    //     email: "test@example.com",
+    //     name: "Test User",
+    //     password: "password123",
+    //   };
 
-    expect(userRecord).toEqual(expectedUser);
-    expect(prismaMock.user.create).toHaveBeenCalledWith({ data: userData });
-  });
+    //   const expectedUser1 = {
+    //     id: 1,
+    //     ...userData1,
+    //     created_at: new Date(),
+    //     updated_at: new Date(),
+    //   };
 
-  // TODO　ユーニーク制約のテストを書く
-  // 下記で同じemaiを登録したらユニークキー制約でエラーになるテストを書いたが、違うemaiで登録してもテストが通ってしまうので改善が必要
-  // it("should throw an error when inserting fails due to unique constraint", async () => {
-  //   const userData1: UserPostRequestBody = {
-  //     email: "test@example.com",
-  //     name: "Test User",
-  //     password: "password123",
-  //   };
+    //   // 最初のユーザーの挿入が成功することをモック
+    //   prismaMock.user.create.mockResolvedValue(expectedUser1);
+    //   const userRecord1 = await userGateway.insert(userData1);
+    //   expect(userRecord1).toEqual(expectedUser1);
+    //   expect(prismaMock.user.create).toHaveBeenCalledWith({ data: userData1 });
 
-  //   const expectedUser1 = {
-  //     id: 1,
-  //     ...userData1,
-  //     created_at: new Date(),
-  //     updated_at: new Date(),
-  //   };
+    //   // 2つ目のユーザーのデータ
+    //   const userData2: UserPostRequestBody = {
+    //     email: "another@example.com", // 異なるメールアドレス
+    //     name: "Another User",
+    //     password: "password456",
+    //   };
 
-  //   // 最初のユーザーの挿入が成功することをモック
-  //   prismaMock.user.create.mockResolvedValue(expectedUser1);
-  //   const userRecord1 = await userGateway.insert(userData1);
-  //   expect(userRecord1).toEqual(expectedUser1);
-  //   expect(prismaMock.user.create).toHaveBeenCalledWith({ data: userData1 });
+    //   // ユニーク制約違反のエラーをモック
+    //   prismaMock.user.create.mockRejectedValue(
+    //     new Error("Unique constraint failed on the fields: (`email`)")
+    //   );
+    //   // 2つ目のユーザーの挿入が失敗することを期待
+    //   await expect(userGateway.insert(userData2)).rejects.toThrow(
+    //     "Unique constraint failed on the fields: (`email`)"
+    //   );
+    // });
+    it("ユーザ作成時にDBエラーで失敗する", async () => {
+      const userData: UserPostRequestBody = {
+        email: "test@example.com",
+        name: "Test User",
+        password: "password123",
+      };
+      prismaMock.user.create.mockRejectedValueOnce(new Error("Database error"));
+      await expect(userGateway.insert(userData)).rejects.toThrow(
+        "Database error"
+      );
+    });
 
-  //   // 2つ目のユーザーのデータ
-  //   const userData2: UserPostRequestBody = {
-  //     email: "another@example.com", // 異なるメールアドレス
-  //     name: "Another User",
-  //     password: "password456",
-  //   };
+    describe("UserGateway getUserByEmail", () => {
+      it("ユーザの取得に成功する", async () => {
+        const email = "test@example.com";
 
-  //   // ユニーク制約違反のエラーをモック
-  //   prismaMock.user.create.mockRejectedValue(
-  //     new Error("Unique constraint failed on the fields: (`email`)")
-  //   );
-  //   // 2つ目のユーザーの挿入が失敗することを期待
-  //   await expect(userGateway.insert(userData2)).rejects.toThrow(
-  //     "Unique constraint failed on the fields: (`email`)"
-  //   );
-  // });
+        const expectedUser = {
+          id: 1,
+          email: "test@example.com",
+          name: "Test User",
+          password: "password123",
+          created_at: new Date(),
+          updated_at: new Date(),
+        };
+        prismaMock.user.findUnique.mockResolvedValue(expectedUser);
+        const userRecord = await userGateway.getUserByEmail(email);
+        expect(userRecord).toEqual(expectedUser);
+        expect(prismaMock.user.findUnique).toHaveBeenCalledWith({
+          where: {
+            email: email,
+          },
+        });
+      });
 
-  it("should throw an error when inserting fails", async () => {
-    const userData: UserPostRequestBody = {
-      email: "test@example.com",
-      name: "Test User",
-      password: "password123",
-    };
+      it("ユーザが存在しない場合、undefinedを返す", async () => {
+        const email = "test@example.com";
+        prismaMock.user.findUnique.mockResolvedValue(null);
+        expect(await userGateway.getUserByEmail(email)).toBe(undefined);
+      });
 
-    prismaMock.user.create.mockRejectedValue(new Error("Database error"));
-
-    await expect(userGateway.insert(userData)).rejects.toThrow(
-      "Database error"
-    );
+      it("ユーザ取得時にDBでエラーが発生した場合にエラーを返す", async () => {
+        prismaMock.user.findUnique.mockRejectedValueOnce(
+          new Error("Database error")
+        );
+        const email = "test@example.com";
+        await expect(userGateway.getUserByEmail(email)).rejects.toThrow(
+          "Database error"
+        );
+      });
+    });
   });
 });
