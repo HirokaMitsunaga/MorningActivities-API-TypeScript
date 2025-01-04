820c67af87b89a8be6e7356cf6884dadd4e94152
hiroka <mitsunagahrk87@gmail.com>
2025-01-04 17:54:17 +0900

    getAllTasksとgetTaskByIdのリポジトリのテスト

--- src/infrastructure/repository/task/taskRepository.test.ts
+++ src/infrastructure/repository/task/taskRepository.test.ts
@@ -2,7 +2,7 @@ import { TaskRepository } from "./taskRepository.js";
 import { TaskEntity } from "../../../domain/task/taskEntity.js";
 import { Task } from "@prisma/client";
 
-describe("LoginUserUsecase Test", () => {
+describe("TaskRepository Test", () => {
   //モックの型定義
   let mockTaskGateway: {
     createTask: jest.Mock<
@@ -14,21 +14,24 @@ describe("LoginUserUsecase Test", () => {
         actualMinutes: number | null
       ]
     >;
-    getAllTasks: jest.Mock<Promise<Task[]>, [userId: number]>;
-    getTaskById: jest.Mock<Promise<Task>, [userId: number, taskId: number]>;
+    getAllTasks: jest.Mock<Promise<Task[] | undefined>, [userId: number]>;
+    getTaskById: jest.Mock<
+      Promise<Task | undefined>,
+      [userId: number, taskId: number]
+    >;
   };
   let taskRepository: TaskRepository;
 
-  const task = new TaskEntity(undefined, "test", 1, 20, 23);
+  const task = new TaskEntity(undefined, "test", 1, 20, 20);
 
-  const expectedDomainTask = new TaskEntity(1, "test", 1, 20, 23);
+  const expectedDomainTask = new TaskEntity(1, "test", 1, 20, 20);
 
   const expectedPrismaTask = {
     id: 1,
     title: "test",
     userId: 1,
     scheduleMinnutes: 20,
-    actualMinutes: 23,
+    actualMinutes: 20,
     createdAt: new Date(),
     updatedAt: new Date(),
   };
@@ -42,24 +45,90 @@ describe("LoginUserUsecase Test", () => {
     };
     taskRepository = new TaskRepository(mockTaskGateway);
   });
-
-  it("タスク作成が成功する", async () => {
-    mockTaskGateway.createTask.mockResolvedValue(expectedPrismaTask);
-    const result = await taskRepository.createTask(task);
-    expect(mockTaskGateway.createTask).toHaveBeenCalledWith(
-      task.title,
-      task.userId,
-      task.scheduleMinnutes ?? null,
-      task.actualMinutes ?? null
-    );
-    expect(result).toEqual(expectedDomainTask);
+  describe("TaskRepository createTask", () => {
+    it("タスク作成が成功する", async () => {
+      mockTaskGateway.createTask.mockResolvedValue(expectedPrismaTask);
+      const result = await taskRepository.createTask(task);
+      expect(mockTaskGateway.createTask).toHaveBeenCalledWith(
+        task.title,
+        task.userId,
+        task.scheduleMinnutes ?? null,
+        task.actualMinutes ?? null
+      );
+      expect(result).toEqual(expectedDomainTask);
+    });
+    it("タスク作成が失敗", async () => {
+      mockTaskGateway.createTask.mockRejectedValueOnce(
+        new Error("Database error")
+      );
+      await expect(taskRepository.createTask(task)).rejects.toThrow(
+        "Database error"
+      );
+    });
   });
-  it("タスク作成が失敗", async () => {
-    mockTaskGateway.createTask.mockRejectedValueOnce(
-      new Error("Database error")
-    );
-    await expect(taskRepository.createTask(task)).rejects.toThrow(
-      "Database error"
-    );
+  describe("TaskRepository getAllTasks", () => {
+    it("タスクの取得に成功する", async () => {
+      const expectedDomainTasks = [
+        new TaskEntity(
+          expectedDomainTask.id,
+          expectedDomainTask.title,
+          expectedDomainTask.userId,
+          expectedDomainTask.scheduleMinnutes,
+          expectedDomainTask.actualMinutes
+        ),
+      ];
+      const expectedPrismaTasks = [expectedPrismaTask];
+      mockTaskGateway.getAllTasks.mockResolvedValue(expectedPrismaTasks);
+      const result = await taskRepository.getAllTasks(task.userId);
+      expect(mockTaskGateway.getAllTasks).toHaveBeenCalledWith(task.userId);
+      expect(result).toEqual(expectedDomainTasks);
+    });
+    it("タスクが存在しない", async () => {
+      mockTaskGateway.getAllTasks.mockResolvedValue(undefined);
+      const result = await taskRepository.getAllTasks(task.userId);
+      expect(result).toEqual(undefined);
+    });
+    it("タスク作成が失敗", async () => {
+      mockTaskGateway.getAllTasks.mockRejectedValueOnce(
+        new Error("Database error")
+      );
+      await expect(taskRepository.getAllTasks(task.userId)).rejects.toThrow(
+        "Database error"
+      );
+    });
+  });
+  describe("TaskRepository getTaskById", () => {
+    const taskData = {
+      userId: 1,
+      taskId: 1,
+    };
+    it("タスクの取得に成功する", async () => {
+      mockTaskGateway.getTaskById.mockResolvedValue(expectedPrismaTask);
+      const result = await taskRepository.getTaskById(
+        taskData.userId,
+        taskData.taskId
+      );
+      expect(mockTaskGateway.getTaskById).toHaveBeenCalledWith(
+        taskData.userId,
+        taskData.taskId
+      );
+      expect(result).toEqual(expectedDomainTask);
+    });
+    it("タスクが存在しない", async () => {
+      mockTaskGateway.getTaskById.mockResolvedValue(undefined);
+      const result = await taskRepository.getTaskById(
+        taskData.userId,
+        taskData.taskId
+      );
+      expect(result).toEqual(undefined);
+    });
+    it("タスク作成が失敗", async () => {
+      mockTaskGateway.getTaskById.mockRejectedValueOnce(
+        new Error("Database error")
+      );
+      await expect(
+        taskRepository.getTaskById(taskData.userId, taskData.taskId)
+      ).rejects.toThrow("Database error");
+    });
   });
 });
