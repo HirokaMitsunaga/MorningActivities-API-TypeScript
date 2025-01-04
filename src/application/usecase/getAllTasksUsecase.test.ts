9984261f32952eb300abe5af145b8ff8f842e426
hiroka <mitsunagahrk87@gmail.com>
2025-01-04 18:09:53 +0900

    getAllTasksとgetTaskByIdのユースケースのテスト

---
+++ src/application/usecase/getAllTasksUsecase.test.ts
@@ -0,0 +1,56 @@
+import { TaskEntity } from "../../domain/task/taskEntity.js";
+import { GetAllTasksUsecase } from "./getAllTasksUsecase.js";
+
+describe("GetAllTasksUsecase Test", () => {
+  let mockTaskRepository: {
+    createTask: jest.Mock<Promise<TaskEntity>, [TaskEntity]>;
+    getAllTasks: jest.Mock<Promise<TaskEntity[] | undefined>, [number]>;
+    getTaskById: jest.Mock<Promise<TaskEntity | undefined>, [number]>;
+  };
+
+  let getAllTasksUsecase: GetAllTasksUsecase;
+
+  const task = new TaskEntity(undefined, "test", 1, 20, 20);
+
+  const expectedTask = new TaskEntity(1, "test", 1, 20, 20);
+
+  beforeEach(() => {
+    mockTaskRepository = {
+      createTask: jest.fn(),
+      getAllTasks: jest.fn(),
+      getTaskById: jest.fn(),
+    };
+    getAllTasksUsecase = new GetAllTasksUsecase(mockTaskRepository);
+  });
+
+  const expectedTasks = [
+    new TaskEntity(
+      expectedTask.id,
+      expectedTask.title,
+      expectedTask.userId,
+      expectedTask.scheduleMinnutes,
+      expectedTask.actualMinutes
+    ),
+  ];
+
+  it("タスク取得が成功する", async () => {
+    mockTaskRepository.getAllTasks.mockResolvedValue(expectedTasks);
+    const result = await getAllTasksUsecase.run(task.userId);
+    expect(result).toBe(expectedTasks);
+    expect(mockTaskRepository.getAllTasks).toHaveBeenCalledWith(task.userId);
+  });
+  it("タスクが存在しない", async () => {
+    mockTaskRepository.getAllTasks.mockResolvedValue(undefined);
+    const result = await getAllTasksUsecase.run(task.userId);
+    expect(result).toBe(undefined);
+  });
+  it("タスク取得が失敗する", async () => {
+    mockTaskRepository.getAllTasks.mockRejectedValueOnce(
+      new Error("Database error")
+    );
+    // const result = await createTaskUsecase.run(task);
+    await expect(getAllTasksUsecase.run(task.userId)).rejects.toThrow(
+      "Database error"
+    );
+  });
+});
---
+++ src/application/usecase/getTaskByIdUsecase.test.ts
@@ -0,0 +1,60 @@
+import { TaskEntity } from "../../domain/task/taskEntity.js";
+import { GetTaskByIdUsecase } from "./getTaskByIdUsecase.js";
+
+describe("GetTaskByIdUsecase Test", () => {
+  let mockTaskRepository: {
+    createTask: jest.Mock<Promise<TaskEntity>, [TaskEntity]>;
+    getAllTasks: jest.Mock<Promise<TaskEntity[] | undefined>, [number]>;
+    getTaskById: jest.Mock<Promise<TaskEntity | undefined>, [number]>;
+  };
+
+  let getTaskByIdUsecase: GetTaskByIdUsecase;
+
+  const task = new TaskEntity(undefined, "test", 1, 20, 20);
+
+  const expectedTask = new TaskEntity(1, "test", 1, 20, 20);
+
+  beforeEach(() => {
+    mockTaskRepository = {
+      createTask: jest.fn(),
+      getAllTasks: jest.fn(),
+      getTaskById: jest.fn(),
+    };
+    getTaskByIdUsecase = new GetTaskByIdUsecase(mockTaskRepository);
+  });
+
+  const taskData = {
+    userId: 1,
+    taskId: 1,
+  };
+
+  it("タスク取得が成功する", async () => {
+    mockTaskRepository.getTaskById.mockResolvedValue(expectedTask);
+    const result = await getTaskByIdUsecase.run(
+      taskData.userId,
+      taskData.taskId
+    );
+    expect(result).toBe(expectedTask);
+    expect(mockTaskRepository.getTaskById).toHaveBeenCalledWith(
+      taskData.userId,
+      taskData.taskId
+    );
+  });
+  it("タスクが存在しない", async () => {
+    mockTaskRepository.getAllTasks.mockResolvedValue(undefined);
+    const result = await getTaskByIdUsecase.run(
+      taskData.userId,
+      taskData.taskId
+    );
+    expect(result).toBe(undefined);
+  });
+  it("タスク取得が失敗する", async () => {
+    mockTaskRepository.getTaskById.mockRejectedValueOnce(
+      new Error("Database error")
+    );
+    // const result = await createTaskUsecase.run(task);
+    await expect(
+      getTaskByIdUsecase.run(taskData.userId, taskData.taskId)
+    ).rejects.toThrow("Database error");
+  });
+});
