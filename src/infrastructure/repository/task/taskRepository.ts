8825a94a096651717e88fd0d8a934284dccb6a1d
hiroka <mitsunagahrk87@gmail.com>
2025-01-04 09:27:54 +0900

    taskRepositoryへtry-catchを追加

--- src/infrastructure/repository/task/taskRepository.ts
+++ src/infrastructure/repository/task/taskRepository.ts
@@ -7,19 +7,25 @@ const taskGateway = new TaskGateway(new PrismaClient());
 
 export class TaskRepository implements TaskRepositoryInterface {
   async createTask(task: Task): Promise<Task> {
-    const taskRes = await taskGateway.createTask(
-      task.title,
-      task.userId,
-      task.scheduleMinnutes ?? null,
-      task.actualMinutes ?? null
-    );
-
-    return new Task(
-      taskRes.id,
-      taskRes.title,
-      taskRes.userId,
-      taskRes.scheduleMinnutes ?? undefined,
-      taskRes.actualMinutes ?? undefined
-    );
+    try {
+      const taskRes = await taskGateway.createTask(
+        task.title,
+        task.userId,
+        task.scheduleMinnutes ?? null,
+        task.actualMinutes ?? null
+      );
+      return new Task(
+        taskRes.id,
+        taskRes.title,
+        taskRes.userId,
+        taskRes.scheduleMinnutes ?? undefined,
+        taskRes.actualMinutes ?? undefined
+      );
+    } catch (error) {
+      if (error instanceof Error) {
+        throw error;
+      }
+      throw new Error("Unknown error occurred while create task");
+    }
   }
 }
