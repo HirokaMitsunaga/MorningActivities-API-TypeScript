8b707a78a1bf8d3ff1ed01b8dff823ac963b0c81
hiroka <mitsunagahrk87@gmail.com>
2025-01-13 08:55:29 +0900

    ログインのDI

--- src/dependencyInjection/types.ts
+++ src/dependencyInjection/types.ts
@@ -2,7 +2,7 @@ const TYPES = {
   LoginUserUsecase: Symbol.for("LoginUserUsecase"),
   UserGateway: Symbol.for("UserGateway"),
   Logger: Symbol.for("Logger"),
-  PrismaClient: Symbol.for("PrismaClient"), // 追加
+  PrismaClient: Symbol.for("PrismaClient"),
 };
 
 export { TYPES };
