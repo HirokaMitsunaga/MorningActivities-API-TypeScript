f4d7f94b7612691fa2e06981fdd58aba7f6017d0
hiroka <mitsunagahrk87@gmail.com>
2025-01-13 17:56:15 +0900

    ミドルウェアの処理を統一

---
+++ src/middleware/appMiddleware.ts
@@ -0,0 +1,17 @@
+import { prettyJSON } from "hono/pretty-json";
+import { corsMiddleware } from "./cors.js";
+import { csrfMiddleware } from "./csrf.js";
+import { every } from "hono/combine";
+import { requestId } from "hono/request-id";
+import { contextStorage } from "hono/context-storage";
+import { createFactory } from "hono/factory";
+
+// factoryの作成
+export const middlewareFactory = createFactory({
+  initApp: (app) => {
+    app.use("*", prettyJSON());
+    app.use("*", contextStorage());
+    app.use("*", requestId());
+    app.use("*", every(corsMiddleware, csrfMiddleware));
+  },
+});
