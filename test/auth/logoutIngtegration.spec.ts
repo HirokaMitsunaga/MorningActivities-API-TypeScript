97eb10b95000bb7b9416962d2379261cf1c96ff4
hiroka <mitsunagahrk87@gmail.com>
2025-01-19 16:45:11 +0900

    ログアウトの統合テストの追加

---
+++ test/auth/logoutIngtegration.spec.ts
@@ -0,0 +1,60 @@
+import { middlewareFactory } from "../../src/middleware/appMiddleware.js";
+import user from "../../src/presentation/auth/userRouter.js";
+
+import { Hono } from "hono";
+
+const correctUserData = {
+  email: "test@example.com",
+  name: "Test User",
+  password: "password123",
+};
+
+const noUserData = {
+  email: "aaa@example.com",
+  name: "Test User",
+  password: "password123",
+};
+
+const incorrectPasswordUserData = {
+  email: "aaa@example.com",
+  name: "Test User",
+  password: "incorrect",
+};
+
+describe("Login integration test", () => {
+  let app: Hono;
+  beforeEach(() => {
+    app = middlewareFactory.createApp().basePath("/api");
+    app.route("/", user);
+  });
+
+  it("Cookieの削除に成功しログアウトが成功する", async () => {
+    //ログイン
+    const loginResponse = await app.request("/api/login", {
+      method: "POST",
+      body: JSON.stringify({
+        email: correctUserData.email,
+        name: correctUserData.name,
+        password: correctUserData.password,
+      }),
+    });
+    expect(loginResponse.status).toBe(201);
+    //ログアウト
+    const logoutResponse = await app.request("/api/logout", {
+      method: "POST",
+      headers: {
+        Cookie: loginResponse.headers.get("Set-Cookie") || "",
+      },
+    });
+    const responseBody = await logoutResponse.json();
+    console.log("Logout response body:", responseBody); // レスポンスの内容を確認
+    expect(logoutResponse.status).toBe(201);
+  });
+  it("Cookieの削除に失敗し、ログアウトが失敗する", async () => {
+    const response = await app.request("/api/logout", {
+      method: "POST",
+    });
+    const responseBody = await response.json();
+    expect(response.status).toBe(500);
+  });
+});
