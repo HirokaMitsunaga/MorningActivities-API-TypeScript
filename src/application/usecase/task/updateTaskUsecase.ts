6ee33c9ff363367f30f90e65f7cc87a15b823bd5
hiroka <mitsunagahrk87@gmail.com>
2025-01-20 09:00:54 +0900

    タスクの更新の時に作成者以外は更新できないように修正

--- src/application/usecase/task/updateTaskUsecase.ts
+++ src/application/usecase/task/updateTaskUsecase.ts
@@ -1,5 +1,6 @@
 import { TaskEntity } from "../../../domain/task/taskEntity.js";
 import { TaskRepositoryInterface } from "../../../domain/task/taskRepositoryInterface.js";
+import { DomainError } from "../../../validator/domainError.js";
 
 export class UpdateTaskUsecase {
   constructor(private _taskRepository: TaskRepositoryInterface) {}
@@ -8,10 +9,21 @@ export class UpdateTaskUsecase {
       if (typeof task.id !== "number") {
         throw new Error("taskId is required and must be a number");
       }
+      const checkTask = await this._taskRepository.getTaskById(
+        task.userId,
+        task.id
+      );
+      if (!checkTask) {
+        throw new DomainError("You can only update your own task");
+      }
+
       const TaskRes = await this._taskRepository.updateTask(task);
       return TaskRes;
     } catch (error) {
       if (error instanceof Error) {
+        throw error;
+      }
+      if (error instanceof DomainError) {
         throw error;
       }
       throw new Error("Unknown error occurred while update task");
